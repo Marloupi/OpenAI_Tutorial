acea34c788c13d092df6c0de8772f286f5ea01f9
Marloupi <marius.sobczak@yahoo.de>
2023-05-20 19:29:43 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/GenerateImageRequest.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class GenerateImageRequest {
+
+//    @NotBlank
+//    private String prompt;
+//
+//    @ValidSize
+//    private String size;
+//
+//    @Min(1)
+//    @Max(10)
+//    @JsonProperty("num_images")
+//    private int numImages;
+}
